5871258bcb9d6dd0909405d00949790b62a7616d
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-06-25 12:26:36 +0300

    ok

---
+++ Lesson02/Assignment.java
@@ -0,0 +1,20 @@
+class Assignment {
+  public static void main (String[] args) {
+    String txt = "Fantastic ";
+    String lang = "Java";
+    txt += lang;
+    System.out.println("Складываем и присваиваем строки: " + txt);
+
+    int sum = 10;
+    int num = 20;
+    sum += num;
+    System.out.println("Складываем и присваиваем целые числа: " + sum);
+
+    int factor = 5;
+    sum *= factor;
+    System.out.println("Результат умножения: " + sum);
+
+    sum /= factor;
+    System.out.println("Результат деления: " + sum);
+  }
+}
