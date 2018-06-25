36d88d088304e1b19e6924e42e01147270f04f3e
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-06-25 12:10:20 +0300

    ok

---
+++ Lesson02/Arithmetic.java
@@ -0,0 +1,19 @@
+class Arithmetic {
+  public static void main (String[] args) {
+    int num = 100;
+    int factor = 20;
+    int sum = 0;
+
+    sum = num + factor;
+    System.out.println("Результат сложения: " + sum);
+
+    sum = num - factor;
+    System.out.println("Результат вычитания: " + sum);
+
+    sum = num * factor;
+    System.out.println("Результат умножения: " + sum);
+
+    sum = num / factor;
+    System.out.println("Результат деления: " + sum);
+  }
+}
