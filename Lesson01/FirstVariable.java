d1901e49c9304e4975076f985c0948e4c386b469
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-06-25 10:39:51 +0300

    ok

---
+++ Lesson01/FirstVariable.java
@@ -0,0 +1,9 @@
+class FirstVariable {
+  public static void main (String[] args) {
+    String message = "Начальное значение";
+    System.out.println(message);
+
+    message = "Изменённое значение";
+    System.out.println(message);
+  }
+}
