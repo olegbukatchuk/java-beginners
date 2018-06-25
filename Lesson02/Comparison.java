a23fda9c96b4ab68157115a4feda014bbcc48c3a
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-06-25 13:15:13 +0300

    ok

--- Lesson02/Comparison.java
+++ Lesson02/Comparison.java
@@ -9,6 +9,13 @@ class Comparison {
     state = (txt != lang);
     System.out.println("Проверка строк на неравенство: " + state);
 
-    
+    int dozen = 12;
+    int score = 20;
+
+    state = (dozen > score);
+    System.out.println("Проверка на больше: " + state);
+
+    state = (dozen < score);
+    System.out.println("Проверка на меньше: " + state);
   }
 }
