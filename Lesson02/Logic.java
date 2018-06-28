8da8f4c24b976959b034fd82cd5e3bc643cb4913
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-06-28 13:58:03 +0300

    ok

---
+++ Lesson02/Logic.java
@@ -0,0 +1,16 @@
+class Logic {
+  public static void main(String[] args) {
+    boolean yes = true;
+    boolean no = false;
+
+    System.out.println("Результат выражения yes И yes: " + (yes && yes));
+    System.out.println("Результат выражения yes И no: " + (yes && no));
+
+    System.out.println("Результат выражения yes ИЛИ yes:" + (yes||yes));
+    System.out.println("Результат выражения yes ИЛИ no: " + (yes || no));
+    System.out.println("Результат выражения no ИЛИ no: " + (no || no));
+
+    System.out.println("Первоначальное значение переменной yes: " + yes);
+    System.out.println("Инвертированная переменная yes: " + !yes);
+  }
+}
