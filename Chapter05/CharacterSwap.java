f8c86e4c92a61fbf433fbe23c344d0650036f39d
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-07-02 16:49:43 +0300

    ok

---
+++ Chapter05/CharacterSwap.java
@@ -0,0 +1,23 @@
+class CharapterSwap {
+  public static void main(String[] args) {
+    String txt = "";
+
+    if (txt.isEmpty()) txt = " Боррокудо ";
+
+    System.out.println("Строка: " + txt);
+    System.out.println("Длина первоначальной строки: " + txt.length());
+
+    txt = txt.trim();
+    System.out.println("Строка: " + txt);
+    System.out.println("Длина строки: " + txt.length());
+
+    char initial = txt.charAt(0);
+    System.out.println("Первая буква: " + initial);
+
+    initial = txt.charAt((txt.length() -1));
+    System.out.println("Последняя буква: " + initial);
+
+    txt = txt.replace('о', 'а' );
+    System.out.println("Строка: " + txt);
+  }
+}
