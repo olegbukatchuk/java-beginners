0ae7d5f4d58b75b62591645735971609cb62f67f
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-07-02 14:55:48 +0300

    ok

--- Chapter03/Loops.java
+++ Chapter03/Loops.java
@@ -1,9 +1,25 @@
 class Loops {
   public static void main(String[] args) {
+  /*
     if (args.length > 0) {
       for (int i = 0; i < args.length; i++) {
         System.out.println("args[" + i + "] - | " + args[i]);
       }
+    }
+  */
+  
+    String[] htm = {
+      "HTML5",
+      "in",
+      "easy",
+      "steps"
+    };
+
+    int j = 0;
+
+    while (j < htm.length) {
+      System.out.println("htm[" + j + "] - | " + htm[j] );
+      j++ ;
     }
   }
 }
