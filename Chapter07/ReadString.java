bd4e6d5aff15484129f75dc950e03ac6118210e7
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-07-03 12:30:55 +0300

    ok

---
+++ Chapter07/ReadString.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class ReadString {
+  public static void main(String[] args) {
+    System.out.print("Введите название данной книги: ");
+
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader buffer = new BufferedReader(isr);
+    String input = "";
+
+    try {
+      input = buffer.readLine();
+      buffer.close();
+    } catch (IOException e) {
+      System.out.println("Произошла ошибка ввода");
+    }
+
+    System.out.println("\nСпасибо, вы читаете " + input);
+  }
+}
