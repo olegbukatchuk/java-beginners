6e5bde42c09fe0028718a0042a4f980634f2a2eb
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-07-16 16:39:24 +0300

    ok

--- Chapter09/States.java
+++ Chapter09/States.java
@@ -37,5 +37,29 @@ class States extends JFrame implements ItemListener {
     pnl.add(chk);
     pnl.add(box);
     pnl.add(txtArea);
+
+    rad1.addItemListener(this);
+    rad2.addItemListener(this);
+    chk.addItemListener(this);
+    box.addItemListener(this);
   }
+
+  public void itemStateChanged(ItemEvent event) {
+    if (event.getItemSelectable() == rad1) {
+      txtArea.setText("Выбрано белое вино");
+
+      if (event.getItemSelectable() == rad2) {
+        txtArea.setText("Выбрано красное вино");
+      }
+    }
+
+    if ((event.getItemSelectable() == chk) && (event.getStateChange() == ItemEvent.SELECTED)) {
+      txtArea.append("\nВыбрана Пеперони\n");
+    }
+
+    if ((event.getItemSelectable() == box) && (event.getStateChange() == ItemEvent.SELECTED)) {
+      txtArea.append("Выбрана" + event.getItem().toString());
+    }
+  }
+
 }
